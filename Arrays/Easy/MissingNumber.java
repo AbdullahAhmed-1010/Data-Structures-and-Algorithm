e585ef806af74d357296af3be212ea0678687471
Abdullah Ahmed <abdullah.ahmed.pisces1010@gmail.com>
2026-08-08 18:33:56 +0530

    find the missing number from the given array

---
+++ Arrays/Easy/MissingNumber.java
@@ -0,0 +1,53 @@
+package Arrays.Easy;
+
+import java.util.Scanner;
+
+public class MissingNumber {
+    //bruteforce solution
+    public static int missingNumber(int[] arr){
+        int n = arr.length+1;
+        for(int i=1; i<=n; i++){
+            int flag = 0;
+            for(int j=0; j<n-1; j++){
+                if(arr[j] == i){
+                    flag = 1;
+                    break;
+                }
+            }
+            if(flag == 0)
+                return i;
+        }
+        return 0;
+    }
+    //better solution -> using hashing
+    public static int betterSolution(int[] arr){
+        int n = arr.length + 1;
+        int[] hash = new int[n+1];
+        for(int  i=0; i<arr.length; i++){
+            hash[arr[i]] = 1;
+        }
+        for(int i=1; i<=n; i++){
+            if(hash[i] == 0)
+                return i;
+        }
+        return 0;
+    }
+    public static void main(String[] args) {
+        Scanner sc = new Scanner(System.in);
+
+        System.out.println("Enter the size of the array: ");
+        int n = sc.nextInt();
+        
+        int arr[] = new int[n-1];
+        System.out.println("Enter the elements of the array: ");
+        for(int i=0; i<n-1; i++){
+            arr[i] = sc.nextInt();
+        }
+
+        // int res = missingNumber(arr);
+        int res = betterSolution(arr);
+        System.out.println(res);
+
+        sc.close();
+    }
+}
