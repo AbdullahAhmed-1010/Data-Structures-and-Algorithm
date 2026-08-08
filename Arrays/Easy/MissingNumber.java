4afe5a4af53284d7ff0749a408db6c306286cdeb
Abdullah Ahmed <abdullah.ahmed.pisces1010@gmail.com>
2026-08-09 00:17:49 +0530

    solved finding the missing element from a given array using summation technique

--- Arrays/Easy/MissingNumber.java
+++ Arrays/Easy/MissingNumber.java
@@ -32,6 +32,19 @@ public class MissingNumber {
         }
         return 0;
     }
+    //optimal solution -> using summation technique
+    public static long optimalsolution1(int[] arr){
+        int n = arr.length+1;
+        long sum1 = (n*(n+1))/2;
+        long sum2 = 0;
+
+        for(int i=0; i<arr.length; i++){
+            sum2 = sum2 + arr[i];
+        }
+        long missingElement = sum1 - sum2;
+        return missingElement;
+    }
+    //another optimal approach is to use XOR bit manipulation method
     public static void main(String[] args) {
         Scanner sc = new Scanner(System.in);
 
@@ -45,7 +58,8 @@ public class MissingNumber {
         }
 
         // int res = missingNumber(arr);
-        int res = betterSolution(arr);
+        // int res = betterSolution(arr);
+        long res = optimalsolution1(arr);
         System.out.println(res);
 
         sc.close();
